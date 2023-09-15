31f4b6f5ca28b4f867943d22b3f22db5b966f69c
logravia <j.alunans@gmail.com>
2023-09-16 00:27:54 +0300

    Prettify the sheetworker.js

--- src/sheetworkers.js
+++ src/sheetworkers.js
@@ -1,5 +1,5 @@
-function advantageFlawStrs(){
- let arr = [];
+function advantageFlawStrs() {
+  let arr = [];
   for (let i = 0; i < 12; i++) {
     arr.push(`advantage_flaw_value${i}`);
   }
@@ -7,39 +7,59 @@ function advantageFlawStrs(){
 }
 
 const SKILLS = [
-  'athletics',    'brawl',
-  'craft',        'driving',
-  'firearms',     'larceny',
-  'melee',        'stealth',
-  'survival',     'animal_ken',
-  'etiquette',    'insight',
-  'intimidation', 'leadership',
-  'performance',  'persuasion',
-  'streetwise',   'subterfuge',
-  'academics',    'awareness',
-  'finance',      'investigation',
-  'medicine',     'occult',
-  'politics',     'science',
-  'technology'
-]
+  "athletics",
+  "brawl",
+  "craft",
+  "driving",
+  "firearms",
+  "larceny",
+  "melee",
+  "stealth",
+  "survival",
+  "animal_ken",
+  "etiquette",
+  "insight",
+  "intimidation",
+  "leadership",
+  "performance",
+  "persuasion",
+  "streetwise",
+  "subterfuge",
+  "academics",
+  "awareness",
+  "finance",
+  "investigation",
+  "medicine",
+  "occult",
+  "politics",
+  "science",
+  "technology",
+];
 const ATTRS = [
-  'strength',
-  'dexterity',
-  'stamina',
-  'charisma',
-  'manipulation',
-  'composure',
-  'intelligence',
-  'wits',
-  'resolve'
-]
-const MAX_ATTR_VALUE = 5
-const OTHER_ATTRS = ["glory", "wisdom", "honor", "rage", "hauglosk", "harano"]
-const HITPOINT_ATTRS = ["health_status", "willpower_status", "crinos_status"]
-const HITPOINT_TYPES = ["health", "willpower", "crinos"]
+  "strength",
+  "dexterity",
+  "stamina",
+  "charisma",
+  "manipulation",
+  "composure",
+  "intelligence",
+  "wits",
+  "resolve",
+];
+const MAX_ATTR_VALUE = 5;
+const OTHER_ATTRS = ["glory", "wisdom", "honor", "rage", "hauglosk", "harano"];
+const HITPOINT_ATTRS = ["health_status", "willpower_status", "crinos_status"];
+const HITPOINT_TYPES = ["health", "willpower", "crinos"];
 const HITPOINT_ORDER = ["empty", "full", "scratch", "grievous"];
 const MAX_HITPOINTS = 10;
-const HITPOINT_TEMPLATE = {empty: 10, full: 0, scratch: 0, grievous: 0, max: 3, bonus: 0}
+const HITPOINT_TEMPLATE = {
+  empty: 10,
+  full: 0,
+  scratch: 0,
+  grievous: 0,
+  max: 3,
+  bonus: 0,
+};
 const ADVANTAGES_FLAWS = advantageFlawStrs();
 
 /**
@@ -54,7 +74,8 @@ function applyClassToDotButtonSet(name, value) {
   const dotOne = $20(`.${name}[value='1']`);
 
   if (value === 0) {
-    dotOne.removeClass("clear-me selected"); return;
+    dotOne.removeClass("clear-me selected");
+    return;
   }
 
   for (let i = 1; i <= MAX_ATTR_VALUE; i++) {
@@ -77,29 +98,30 @@ function applyClassToDotButtonSet(name, value) {
  * @returns {boolean} True if it's a reset click, otherwise false.
  */
 function resetClick(hist, clickedAttrName, clickedAttrVal) {
-  const prevName = hist.click_history.name
-  const prevVal = hist.click_history.val
-  const isReset = prevName === clickedAttrName && prevVal === 1 && clickedAttrVal === 1
+  const prevName = hist.click_history.name;
+  const prevVal = hist.click_history.val;
+  const isReset =
+    prevName === clickedAttrName && prevVal === 1 && clickedAttrVal === 1;
 
   return isReset;
 }
 
 /**
  * @returns {Array} names of all the dot attributes such as resolve, driving, hauglosk etc*/
-function dotAttributes(){
-  return SKILLS.concat(ATTRS).concat(OTHER_ATTRS).concat(ADVANTAGES_FLAWS)
+function dotAttributes() {
+  return SKILLS.concat(ATTRS).concat(OTHER_ATTRS).concat(ADVANTAGES_FLAWS);
 }
 
 /**
  * Restores class attributes for dot sets in the sheet.
  * This function retrieves data for dot attributes and applies them to corresponding dot sets.*/
-function restoreDotAttributeClasses(){
-  getAttrs(dotAttributes(), (names_values)=> {
+function restoreDotAttributeClasses() {
+  getAttrs(dotAttributes(), (names_values) => {
     let keys = Object.keys(names_values);
-    keys.forEach(key=>{
-      applyClassToDotButtonSet(key, names_values[key])
-    })
-  })
+    keys.forEach((key) => {
+      applyClassToDotButtonSet(key, names_values[key]);
+    });
+  });
 }
 
 /**
@@ -109,24 +131,23 @@ function restoreDotAttributeClasses(){
  * When clicked, it updates the value of a dot button and tracks the click history.
  */
 function setUpDotValueButton() {
-  $20('.dot-value-button').on('click', e => {
+  $20(".dot-value-button").on("click", (e) => {
     let clickedDotValue = parseInt(e.htmlAttributes.value);
     let name = e.htmlAttributes["data-name"];
 
-    getAttrs(["click_history"], hist => {
+    getAttrs(["click_history"], (hist) => {
       if (resetClick(hist, name, clickedDotValue)) {
         clickedDotValue = 0;
       }
 
       applyClassToDotButtonSet(name, clickedDotValue);
 
-      setAttrs(
-        {
-          [name]: clickedDotValue,
-          click_history: { name: name, val: clickedDotValue }
-        });
-    })
-  })
+      setAttrs({
+        [name]: clickedDotValue,
+        click_history: { name: name, val: clickedDotValue },
+      });
+    });
+  });
 }
 
 /**
@@ -134,11 +155,11 @@ function setUpDotValueButton() {
  *
  * @param {string} name - The name of the element group to clear hitpoint boxes.
  */
-function clearHitpointBoxes(name){
-  let classesToRemoveStr = HITPOINT_ORDER.join(" ")
+function clearHitpointBoxes(name) {
+  let classesToRemoveStr = HITPOINT_ORDER.join(" ");
 
   for (let i = 1; i <= MAX_HITPOINTS; i++) {
-    $20(`.${name}`).removeClass(classesToRemoveStr)
+    $20(`.${name}`).removeClass(classesToRemoveStr);
   }
 }
 /**
@@ -148,38 +169,42 @@ function clearHitpointBoxes(name){
  * @param {object} status - The status object containing full, scratch, grievous, and empty values.
  */
 function styleHitpointBoxes(name, status) {
-  clearHitpointBoxes(name)
+  clearHitpointBoxes(name);
 
-  let i = 1
-  for(; i<=status.full; i++){
-    $20(`.${name}[value="${i}"]`).addClass("full")
+  let i = 1;
+  for (; i <= status.full; i++) {
+    $20(`.${name}[value="${i}"]`).addClass("full");
   }
 
-  for(; i<=status.scratch + status.full; i++){
-    $20(`.${name}[value="${i}"]`).addClass("scratch")
+  for (; i <= status.scratch + status.full; i++) {
+    $20(`.${name}[value="${i}"]`).addClass("scratch");
   }
 
-  for(; i<=status.grievous + status.scratch + status.full; i++){
-    $20(`.${name}[value="${i}"]`).addClass("grievous")
+  for (; i <= status.grievous + status.scratch + status.full; i++) {
+    $20(`.${name}[value="${i}"]`).addClass("grievous");
   }
-  for(; i<=status.grievous + status.scratch + status.full + status.empty; i++){
-    $20(`.${name}[value="${i}"]`).addClass("empty")
+  for (
+    ;
+    i <= status.grievous + status.scratch + status.full + status.empty;
+    i++
+  ) {
+    $20(`.${name}[value="${i}"]`).addClass("empty");
   }
 }
 
 /** Restores styles for health and willpower */
-function restoreHitpointStyles(){
-  getAttrs(HITPOINT_ATTRS, vals=>{
-    HITPOINT_ATTRS.forEach((attr,i)=>{
-      styleHitpointBoxes(HITPOINT_TYPES[i], vals[attr])
-    })
-  })
+function restoreHitpointStyles() {
+  getAttrs(HITPOINT_ATTRS, (vals) => {
+    HITPOINT_ATTRS.forEach((attr, i) => {
+      styleHitpointBoxes(HITPOINT_TYPES[i], vals[attr]);
+    });
+  });
 }
 /** Hitpoint box value on a click should rotate from empty>full>scratch>grievous>empty
  * @param {string} curState
  * @return {string} next rotation value based on curState
  * */
-function nextHitpointState(curState){
+function nextHitpointState(curState) {
   const currentIndex = HITPOINT_ORDER.indexOf(curState);
   const nextIndex = (currentIndex + 1) % HITPOINT_ORDER.length;
   return HITPOINT_ORDER[nextIndex];
@@ -189,32 +214,32 @@ function nextHitpointState(curState){
  * Set up click event handling for health and willpower buttons.
  */
 function setUpHealthWillButton() {
-  $20(".health-will-button").on("click", e => {
+  $20(".health-will-button").on("click", (e) => {
     // Extract data from the clicked element
     let name = e.htmlAttributes["data-name"];
-    let curHitpointState = e.htmlAttributes.class.split(" ").pop() // last class is assumed be injury type, very brittle
-    let attrStr = `${name}_status`
+    let curHitpointState = e.htmlAttributes.class.split(" ").pop(); // last class is assumed be injury type, very brittle
+    let attrStr = `${name}_status`;
     let clickVal = parseInt(e.htmlAttributes.value);
 
-    getAttrs([attrStr], vals => {
-      let status = vals[attrStr]
+    getAttrs([attrStr], (vals) => {
+      let status = vals[attrStr];
 
       // Allow hitbox count to be reduced if clicked on one
       if (clickVal === 1 && status.full > 1) {
-        status.full -= 1
-        status.empty += 1
+        status.full -= 1;
+        status.empty += 1;
       } else {
         // if you click on a hitpoint box, its value disappears and gets replaced by the value in the next rotation
-        status[curHitpointState] -= 1
-        status[nextHitpointState(curHitpointState)] += 1
+        status[curHitpointState] -= 1;
+        status[nextHitpointState(curHitpointState)] += 1;
       }
 
       styleHitpointBoxes(name, status);
       setAttrs({
-        [attrStr] : status
-      })
-    })
-  })
+        [attrStr]: status,
+      });
+    });
+  });
 }
 
 /**
@@ -222,73 +247,83 @@ function setUpHealthWillButton() {
  * This ensures that Roll20 saves a proper and empty JSON object to their Firebase.
  * @param {boolean} [reset=false] - Whether to reset the data.
  * */
-function initHealthWillCrinos(reset=false) {
-  let hitpointContainers = ["health_status", "willpower_status", "crinos_status"];
-  let containersToInit = {}
+function initHealthWillCrinos(reset = false) {
+  let hitpointContainers = [
+    "health_status",
+    "willpower_status",
+    "crinos_status",
+  ];
+  let containersToInit = {};
 
-  getAttrs(hitpointContainers, vals=>{
-    hitpointContainers.forEach(name=>{
+  getAttrs(hitpointContainers, (vals) => {
+    hitpointContainers.forEach((name) => {
       if (vals[name] === "empty" || reset) {
         containersToInit[name] = HITPOINT_TEMPLATE;
       }
-    })
+    });
     if (Object.keys(containersToInit).length !== 0) {
-      setAttrs(containersToInit)
+      setAttrs(containersToInit);
     }
-  })
+  });
 }
 
-function damaged(health){
-  return health.scratch > 0 || health.grievous > 0
+function damaged(health) {
+  return health.scratch > 0 || health.grievous > 0;
 }
 
-function setUpTabButtons(){
-  $20(".tab-button").on("click", e=>{
-    let tabName = e.htmlAttributes.value
+function setUpTabButtons() {
+  $20(".tab-button").on("click", (e) => {
+    let tabName = e.htmlAttributes.value;
     //All tabs get removed .active class
-    $20(".tab").removeClass("active")
+    $20(".tab").removeClass("active");
     //The clicked tab gets assigned .active class, thus becomes visible
-    $20(`.${tabName}`).addClass("active")
-  })
+    $20(`.${tabName}`).addClass("active");
+  });
 }
-
+/**
+ * Initializes various UI elements for onclick events.
+ * Makes sure the UI elements are styled according to the saved data*/
 on("sheet:opened", () => {
   initHealthWillCrinos();
   setUpDotValueButton();
   setUpHealthWillButton();
-  restoreDotAttributeClasses()
+  restoreDotAttributeClasses();
   restoreHitpointStyles();
   setUpTabButtons();
 });
 
-on("change:stamina", ()=>{
-  getAttrs(["health_status", "stamina"], vals=>{
-    if(damaged(vals.health_status)){return} // Only changes health hitpoint on full hitpoints
+on("change:stamina", () => {
+  getAttrs(["health_status", "stamina"], (vals) => {
+    if (damaged(vals.health_status)) {
+      return;
+    } // Only changes health hitpoint on full hitpoints
 
-    let health = vals.health_status
-    health.max = vals.stamina + 3
-    health.full = health.max
-    health.empty = 10 - health.max
+    let health = vals.health_status;
+    health.max = vals.stamina + 3;
+    health.full = health.max;
+    health.empty = 10 - health.max;
 
-    styleHitpointBoxes("health", health)
-    setAttrs({health_status: health});
-  })
-})
+    styleHitpointBoxes("health", health);
+    setAttrs({ health_status: health });
+  });
+});
 
 on("change:resolve change:composure", () => {
-  getAttrs(["willpower_status", "resolve", "composure"], vals => {
-    if (damaged(vals.willpower_status)) { return } //Only change willpower on full hitpoints
+  getAttrs(["willpower_status", "resolve", "composure"], (vals) => {
+    if (damaged(vals.willpower_status)) {
+      return;
+    } //Only change willpower on full hitpoints
 
-    let composure = vals.composure
-    let resolve = vals.resolve
-    let willpower = vals.willpower_status
+    let composure = vals.composure;
+    let resolve = vals.resolve;
+    let willpower = vals.willpower_status;
 
-    willpower.max = composure + resolve
+    willpower.max = composure + resolve;
 
-    willpower.full = willpower.max
-    willpower.empty = 10 - willpower.max
+    willpower.full = willpower.max;
+    willpower.empty = 10 - willpower.max;
 
-    styleHitpointBoxes("willpower", willpower)
+    styleHitpointBoxes("willpower", willpower);
     setAttrs({ willpower_status: willpower });
-  })
-})
+  });
+});
